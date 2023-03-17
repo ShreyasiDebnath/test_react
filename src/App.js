28fe514ae1db466ec428f3fda8b746760870f966
ShreyasiDebnath <shreyasidebnath07@gmail.com>
2023-03-18 00:47:48 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
- import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Dsapage from './components/Dsapage';
 import Array from './components/Array';
 import About from './components/About';
 import Footer from './components/Footer';
-import Linked from './components/Linked';
-import Stack from './components/Stack';
-import Queue from './components/Queue';
+
 function App() {
   
   return (
