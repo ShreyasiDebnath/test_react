ce78201c7e1f0277fb7bcc3398700a0f218a9cf4
ShreyasiDebnath <shreyasidebnath07@gmail.com>
2023-03-18 00:29:23 +0530

    added

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Alert(props) {
   return (
    
-        props.alert && <div className="alert alert-success" role="alert" style={{marginTop:"70px"}}>
+        props.alert && <div className="alert alert-success" role="alert" style={{marginTop:"60px"}}>
             {props.alert.type}:{props.alert.msg}
         </div>
     
