a9e9fcc9e59db209fc165e4dee253f2725680384
ShreyasiDebnath <shreyasidebnath07@gmail.com>
2023-03-18 00:45:41 +0530

    added

--- src/App.js
+++ src/App.js
@@ -13,49 +13,20 @@ import Linked from './components/Linked';
 import Stack from './components/Stack';
 import Queue from './components/Queue';
 function App() {
-  const [mode,setMode] = useState('black');
-  const [alert,setAlert] =useState(null);
-  const showAlert = (message,type)=>{
-    setAlert({
-      msg: message,
-      type: type
-    })
-    setTimeout(()=>{
-      setAlert(null)
-
-    },1500);
-  }
-  const toggleMode = ()=>{
-    if(mode==='black' ){
-      setMode('white')
-      
-      document.body.style.backgroundColor='black';
-     
-      showAlert("Dark Mode Has Been Enable","Success");
-    }
-    else{
-      setMode('black')
- 
-      document.body.style.backgroundColor='white';
-      showAlert("Light Mode Has Been Enable","Success");
-    }
-  }
- 
+  
   return (
     
      <>
      
-     < Navbar toggleMode={toggleMode}/>
-     <Alert alert={alert}/> 
+     < Navbar />
+   
     <Routes>
           
       <Route exact path='/' element={<Home/>}/>
       <Route exact path='/dsa' element={<Dsapage/>}/>
       <Route exact path='/about' element={<About />}/>
       <Route exact path='/dsa/array' element={<Array/>}/>
-      <Route exact path='/dsa/linked' element={<Linked/>}/>
-      <Route exact path='/dsa/stack' element={<Stack/>}/>
-      <Route exact path='/dsa/queue' element={<Queue/>}/>
+   
       
           
        </Routes>  
